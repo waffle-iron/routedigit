f5a9a65d1ddf3c50300a6806a660f840c73db7e4
Bartholomew Krystman <bkrystman@protonmail.ch>
2016-11-17 21:13:27 -0700

    more data added

--- app/scripts/controllers/portfolio.js
+++ app/scripts/controllers/portfolio.js
@@ -8,10 +8,70 @@
  * Controller of the siteAppApp
  */
 angular.module('siteAppApp')
-  .controller('PortfolioCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('PortfolioCtrl', function ($scope) {
+
+    var portfolioList = [
+      {
+        id: 1,
+        title: "one",
+        image: "linkOne",
+        description: "desOne",
+        techUsed: "techOne",
+        site: "siteOne",
+        github: "siteGit",
+        youtube: "youtubeOne"
+      },
+      {
+        id: 2,
+        title: "two",
+        image: "linkTwo",
+        description: "desTwo",
+        techUsed: "techTwo",
+        site: "siteTwo",
+        github: "siteTwo",
+        youtube: "youtubeTwo"
+      },
+      {
+        id: 3,
+        title: "two",
+        image: "linkTwo",
+        description: "desTwo",
+        techUsed: "techTwo",
+        site: "siteTwo",
+        github: "siteTwo",
+        youtube: "youtubeTwo"
+      },
+      {
+        id: 4,
+        title: "two",
+        image: "linkTwo",
+        description: "desTwo",
+        techUsed: "techTwo",
+        site: "siteTwo",
+        github: "siteTwo",
+        youtube: "youtubeTwo"
+      },
+      {
+        id: 5,
+        title: "two",
+        image: "linkTwo",
+        description: "desTwo",
+        techUsed: "techTwo",
+        site: "siteTwo",
+        github: "siteTwo",
+        youtube: "youtubeTwo"
+      },
+      {
+        id: 6,
+        title: "two",
+        image: "linkTwo",
+        description: "desTwo",
+        techUsed: "techTwo",
+        site: "siteTwo",
+        github: "siteTwo",
+        youtube: "youtubeTwo"
+      }
+    ]
+
+
   });
